ff0891adcef8ce08c9755cdc629017d792dcc303
SalvatierraFran <franco14_93@hotmail.com>
2016-04-19 01:04:42 -0300

    Creadas las variables indicadas por la guia y cargado el primer objeto en la lista.

---
+++ Jugador.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio27
+{
+    class Jugador
+    {
+        public int golesConvertidos;
+        public string nombre;
+        public string nombreEquipo;
+        public int partidosJugados;
+        public float promedioDeGol;
+
+        public List<Jugador> ListaDeJugadores;
+
+        public Jugador(int Goles, string Nombre, string Equipo, int PJ)
+        {
+            this.golesConvertidos = Goles;
+            this.nombre = Nombre;
+            this.nombreEquipo = Equipo;
+            this.partidosJugados = PJ;
+
+            this.ListaDeJugadores = new List<Jugador>();
+        }
+
+        public void Agregar(Jugador UnJugador)
+        {
+            this.ListaDeJugadores.Add(UnJugador);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Jugador UnJugador = new Jugador(10, "Franco", "River", 15);
+
+            UnJugador.Agregar(UnJugador);
+
+            Console.ReadKey();
+        }
+    }
+}
